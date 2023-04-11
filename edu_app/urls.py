e8d1efbcc4dd140e9f2f3cf63fcd55c0f25f1d74
ashiqe123 <muhammedashiqva@gmail.com>
2023-04-11 16:23:55 +0530

    secondcommit

--- edu_app/urls.py
+++ edu_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns=[
     
 path('',views.index,name='index'),
-path('courses',views.course,name='course')
+path('courses',views.course,name='course'),
+path('Gallery',views.gallery,name='gallery'),
+path('placement',views.placement,name='placement'),
+path('contact',views.contact,name='contact')
 ]
--- edu_app/views.py
+++ edu_app/views.py
@@ -6,3 +6,12 @@ def index(request):
 
 def course(request):
     return render(request,'course.html')
+
+def gallery(request):
+    return render(request,'gallery.html')
+
+def placement(request):
+    return render(request,'placement.html')
+
+def contact(request):
+    return render(request,'contact.html')
